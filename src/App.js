985b8867008671270c42984313d62844e0145b3e
Somjeet Srimani <somjeetsrimani2000@gmail.com>
2024-11-21 15:58:26 +0530

    Implement the Router in the navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
 
+/*
+Very Important:-
+1. In the latest react-router-dom v6.24.1 you have to use <Routes></Routes> instead of <Switch>
+2. If you want to show navbar at the top, create <route path="/" element={<Navbar />} ... </Route>
+    Between ... you can route your other components, like Home, About etc.
+3. When use <Link> tag instead of <a> tag, you have to use <Outlet /> method at the very end of the file and also import it, It comes from react-router-dom package.
+    Use-case - Mainly in Navbar, Footer like areas where we have mention other end-points.
+*/
+
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-      
+    <div className='App'>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Navbar />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,41 +1,74 @@
-import React from 'react'
+import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">News App</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><hr class="dropdown-divider"/></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
+    <>
+      <nav className='navbar navbar-expand-lg bg-body-tertiary'>
+        <div className='container-fluid'>
+          <Link className='navbar-brand' to='/'>
+            NewsApp
+          </Link>
+          <button
+            className='navbar-toggler'
+            type='button'
+            data-bs-toggle='collapse'
+            data-bs-target='#navbarSupportedContent'
+            aria-controls='navbarSupportedContent'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
+          >
+            <span className='navbar-toggler-icon'></span>
+          </button>
+          <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+            <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
+              <li className='nav-item'>
+                <Link className='nav-link active' aria-current='page' to='/'>
+                  Home
+                </Link>
+              </li>
+              <li className='nav-item'>
+                <Link className='nav-link' to='/business'>
+                  Business
+                </Link>
+              </li>
+              <li className='nav-item'>
+                <Link className='nav-link' to='/entertainment'>
+                  Entertainment
+                </Link>
+              </li>
+              <li className='nav-item'>
+                <Link className='nav-link' to='/general'>
+                  General
+                </Link>
+              </li>
+              <li className='nav-item'>
+                <Link className='nav-link' to='/health'>
+                  Health
+                </Link>
+              </li>
+              <li className='nav-item'>
+                <Link className='nav-link' to='/science'>
+                  Science
+                </Link>
+              </li>
+              <li className='nav-item'>
+                <Link className='nav-link' to='/sports'>
+                  Sports
+                </Link>
+              </li>
+              <li className='nav-item'>
+                <Link className='nav-link' to='/technology'>
+                  Technology
+                </Link>
+              </li>
             </ul>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-          </li>
-        </ul>
-        
-      </div>
-    </div>
-  </nav>
-  )
-}
+          </div>
+        </div>
+      </nav>
+      <Outlet />
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
